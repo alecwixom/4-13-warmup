f5f2dc08a0716ad688de7131c2de7538ca03946f
Alec Wixom <atwixom@gmail.com>
2023-04-13 10:33:40 -0600

    update1

---
+++ drinkins-age.js
@@ -0,0 +1,14 @@
+let age = 21
+if (age < 14){
+    console.log("drink toddy");
+} else if(age < 18){
+    console.log("drink coke");
+} else if(age < 21){
+    console.log("drink beer");
+} else {
+    console.log("drink whisky");
+}
+
+// age < 14 ? console.log("you should drink toddy"):
+// age < 18 ? console.log("you will drink coke"):
+// age < 21 ? console.log("you can drink beer now"): console.log("you can drink whisky now")
